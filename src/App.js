55c2ec90ebad9a057f1db6984b37e003bbdfc9aa
Mohan Vaddella <84492994+MohanVaddella@users.noreply.github.com>
2024-08-07 16:34:15 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+import HomePage from './components/HomePage';
+import AddPokemon from './components/AddPokemon';
+import AddPokemonUser from './components/AddPokemonUser';
+import ListPokemonUsers from './components/ListPokemonUsers';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/add-pokemon">Add Pokemon</Link></li>
+              <li><Link to="/add-pokemon-user">Add Pokemon to User</Link></li>
+              <li><Link to="/list-pokemon-users">List Pokemon Users</Link></li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/add-pokemon" element={<AddPokemon />} />
+            <Route path="/add-pokemon-user" element={<AddPokemonUser />} />
+            <Route path="/list-pokemon-users" element={<ListPokemonUsers />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
